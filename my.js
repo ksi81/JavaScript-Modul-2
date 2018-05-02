4ea9faefa45521dac048d0d55ad501a91e3070bf
Serhii Klymenko <klimenko81@gmail.com>
2018-05-02 22:36:12 +0300

    my home work2

--- my.js
+++ my.js
@@ -8,25 +8,29 @@ let inRange = false;
 
 do {
 userInput = prompt('Дай число от больше либо равно 0',`0`);
-const isValidInput = userInput !== null && !Number.isNaN(userInput) && userInput >=0;
-console.log(isValidInput);
-// Необходимо проверить является ли введенные данные целым положительным числом. 
-console.log(userInput);
+const isValidInput = userInput !== null && !Number.isNaN(userInput) && userInput >=0  && !inRange;
+console.log(prompt);
+// console.log(userInput);
 if (isValidInput) {
   // console.log(isValidInput);
-        if(userInput !== null) {
-          console.log('ДА!');
+
+        if(isValidInput && userInput !== null) {
+          // console.log('ДА!');
           numbers.push(userInput);
           console.log(numbers);
             } else {
-              console.log('нажат cancel');
+              alert('Было введено не число');
+              // console.log('нажат cancel');
             }
         }
         else {
-          alert('Было введено не число');
+          console.log('нажат cancel');
+          // alert('Было введено не число');
         }  
 } while (userInput !== null && !inRange);
-alert('Вы закончили ввод');
+
+
+// alert('Вы закончили ввод');
 let i ;
 let sum = 0;
 for (i=0; i < numbers.length; i++)
@@ -34,4 +38,4 @@ for (i=0; i < numbers.length; i++)
 sum += Number(numbers[i]);
 }
 console.log(sum);
-alert(`Общая сумма всех элементов массива: ${sum}`);
+alert(`Вы закончили ввод. Общая сумма всех элементов массива: ${sum}`);
